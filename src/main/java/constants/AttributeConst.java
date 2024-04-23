8b0974086fc906e686717c3577faf159d68c1f91
nori.motoki <1102bad@gmail.com>
2024-04-23 15:36:27 +0900

    定数ファイルを作成

--- src/main/java/constants/AttributeConst.java
+++ src/main/java/constants/AttributeConst.java
@@ -10,12 +10,10 @@ public enum AttributeConst {
     FLUSH("flush"),
 
     //一覧画面共通
-    MAX_ROW("maxRow"),
-    PAGE("page"),
+    MAX_ROW("maxRow"), PAGE("page"),
 
     //入力フォーム共通
-    TOKEN("_token"),
-    ERR("errors"),
+    TOKEN("_token"), ERR("errors"),
 
     //ログイン中の従業員
     LOGIN_EMP("login_employee"),
@@ -24,31 +22,18 @@ public enum AttributeConst {
     LOGIN_ERR("loginError"),
 
     //従業員管理
-    EMPLOYEE("employee"),
-    EMPLOYEES("employees"),
-    EMP_COUNT("employees_count"),
-    EMP_ID("id"),
-    EMP_CODE("code"),
-    EMP_PASS("password"),
-    EMP_NAME("name"),
-    EMP_ADMIN_FLG("admin_flag"),
+    EMPLOYEE("employee"), EMPLOYEES("employees"), EMP_COUNT("employees_count"), EMP_ID("id"), EMP_CODE(
+            "code"), EMP_PASS("password"), EMP_NAME("name"), EMP_ADMIN_FLG("admin_flag"),
 
     //管理者フラグ
-    ROLE_ADMIN(1),
-    ROLE_GENERAL(0),
+    ROLE_ADMIN(1), ROLE_GENERAL(0),
 
     //削除フラグ
-    DEL_FLAG_TRUE(1),
-    DEL_FLAG_FALSE(0),
+    DEL_FLAG_TRUE(1), DEL_FLAG_FALSE(0),
 
     //日報管理
-    REPORT("report"),
-    REPORTS("reports"),
-    REP_COUNT("reports_count"),
-    REP_ID("id"),
-    REP_DATE("report_date"),
-    REP_TITLE("title"),
-    REP_CONTENT("content_msg");
+    REPORT("report"), REPORTS("reports"), REP_COUNT("reports_count"), REP_ID("id"), REP_DATE("report_date"), REP_TITLE(
+            "title"), REP_CONTENT("content_msg");
 
     private final String text;
     private final Integer i;
--- src/main/java/constants/PropertyConst.java
+++ src/main/java/constants/PropertyConst.java
@@ -10,6 +10,7 @@ public enum PropertyConst {
     PEPPER("pepper");
 
     private final String text;
+
     private PropertyConst(final String text) {
         this.text = text;
     }
